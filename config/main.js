768e37490ae028277700ca5a30f76a8e9076865b
totallynotharry <162619834+totallynotharry@users.noreply.github.com>
2026-01-08 21:15:08 +0000

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "🍌 banana arcade"; // Change this to change the name of your website.
-var subtext = ""; // set the subtext
+var subtext = ""the gucci of game websites" -me"; // set the subtext
 
 // more settings in main.css
 
